1342ab3f41e48463dfbb3ba8b3a217992c58e2c6
Avinash <avinashghare572@gmail.com>
2015-04-07 18:13:11 +0530

    database file

--- predicto (1).sql
+++ predicto (1).sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 07, 2015 at 09:38 AM
+-- Generation Time: Apr 07, 2015 at 02:41 PM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.19
 
@@ -82,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `menu` (
   `order` int(11) NOT NULL,
   `icon` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 --
 -- Dumping data for table `menu`
@@ -90,7 +90,16 @@ CREATE TABLE IF NOT EXISTS `menu` (
 
 INSERT INTO `menu` (`id`, `name`, `description`, `keyword`, `url`, `linktype`, `parent`, `isactive`, `order`, `icon`) VALUES
 (1, 'Users', '', '', 'site/viewusers', 1, 0, 1, 1, 'icon-user'),
-(4, 'Dashboard', '', '', 'site/index', 1, 0, 1, 0, 'icon-dashboard');
+(4, 'Dashboard', '', '', 'site/index', 1, 0, 1, 0, 'icon-dashboard'),
+(5, 'Prediction', '', '', 'site/viewprediction', 1, 0, 1, 2, 'icon-user'),
+(6, 'Prediction Group', '', '', 'site/viewpredictiongroup', 1, 0, 1, 3, 'icon-user'),
+(7, 'Prediction Hash', '', '', 'site/viewpredictionhash', 1, 0, 1, 4, 'icon-user'),
+(8, 'Prediction Team', '', '', 'site/viewpredictionteam', 1, 0, 1, 5, 'icon-user'),
+(9, 'Team Group', '', '', 'site/viewteamgroup', 1, 0, 1, 6, 'icon-user'),
+(10, 'User Points Log', '', '', 'site/viewuserpointlog', 1, 0, 1, 7, 'icon-user'),
+(11, 'User Prediction', '', '', 'site/viewuserprediction', 1, 0, 1, 8, 'icon-user'),
+(12, 'User Share', '', '', 'site/viewusershare', 1, 0, 1, 9, 'icon-user'),
+(14, 'Social Login', '', '', 'site/viewsociallogin', 1, 0, 1, 11, 'icon-user');
 
 -- --------------------------------------------------------
 
@@ -120,7 +129,9 @@ INSERT INTO `menuaccess` (`menu`, `access`) VALUES
 (9, 1),
 (10, 1),
 (11, 1),
-(12, 1);
+(12, 1),
+(13, 1),
+(14, 1);
 
 -- --------------------------------------------------------
 
@@ -137,7 +148,15 @@ CREATE TABLE IF NOT EXISTS `predicto_prediction` (
   `endtime` varchar(255) NOT NULL,
   `order` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `predicto_prediction`
+--
+
+INSERT INTO `predicto_prediction` (`id`, `predictiongroup`, `name`, `status`, `predictionteam`, `endtime`, `order`) VALUES
+(1, 1, 'Prediction 1', 1, 1, '10 AM', 1),
+(2, 1, 'demo', 0, 1, '9Am', 2);
 
 -- --------------------------------------------------------
 
@@ -152,7 +171,15 @@ CREATE TABLE IF NOT EXISTS `predicto_predictiongroup` (
   `status` int(11) NOT NULL,
   `endtime` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `predicto_predictiongroup`
+--
+
+INSERT INTO `predicto_predictiongroup` (`id`, `name`, `order`, `status`, `endtime`) VALUES
+(1, 'Group 1', 1, 1, '11 AM'),
+(2, 'Prediction Group 2', 2, 0, '6PM');
 
 -- --------------------------------------------------------
 
@@ -165,7 +192,15 @@ CREATE TABLE IF NOT EXISTS `predicto_predictionhash` (
   `prediction` int(11) NOT NULL,
   `hashtag` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `predicto_predictionhash`
+--
+
+INSERT INTO `predicto_predictionhash` (`id`, `prediction`, `hashtag`) VALUES
+(1, 1, 'Hash Value'),
+(2, 2, 'hashtag prediction demo');
 
 -- --------------------------------------------------------
 
@@ -179,7 +214,15 @@ CREATE TABLE IF NOT EXISTS `predicto_predictionteam` (
   `teamgroup` int(11) NOT NULL,
   `order` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `predicto_predictionteam`
+--
+
+INSERT INTO `predicto_predictionteam` (`id`, `prediction`, `teamgroup`, `order`) VALUES
+(1, 1, 1, 1),
+(2, 2, 2, 3);
 
 -- --------------------------------------------------------
 
@@ -198,7 +241,7 @@ CREATE TABLE IF NOT EXISTS `predicto_sociallogin` (
 --
 
 INSERT INTO `predicto_sociallogin` (`id`, `name`) VALUES
-(1, 'Facebook');
+(1, 'Facebook1');
 
 -- --------------------------------------------------------
 
@@ -212,7 +255,16 @@ CREATE TABLE IF NOT EXISTS `predicto_teamgroup` (
   `predictiongroup` int(11) NOT NULL,
   `order` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `predicto_teamgroup`
+--
+
+INSERT INTO `predicto_teamgroup` (`id`, `name`, `predictiongroup`, `order`) VALUES
+(1, 'Team Group 1', 1, 0),
+(2, 'Team Group 2', 1, 1),
+(3, 'Team Group 3', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -227,7 +279,15 @@ CREATE TABLE IF NOT EXISTS `predicto_userpointlog` (
   `prediction` int(11) NOT NULL,
   `shareid` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `predicto_userpointlog`
+--
+
+INSERT INTO `predicto_userpointlog` (`id`, `point`, `for`, `prediction`, `shareid`) VALUES
+(1, '10', 1, 1, '1235'),
+(2, '100', 0, 1, '123333');
 
 -- --------------------------------------------------------
 
@@ -241,7 +301,14 @@ CREATE TABLE IF NOT EXISTS `predicto_userprediction` (
   `teamgroup` int(11) NOT NULL,
   `prediction` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `predicto_userprediction`
+--
+
+INSERT INTO `predicto_userprediction` (`id`, `user`, `teamgroup`, `prediction`) VALUES
+(1, 4, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -256,7 +323,16 @@ CREATE TABLE IF NOT EXISTS `predicto_usershare` (
   `total` varchar(255) NOT NULL,
   `prediction` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Dumping data for table `predicto_usershare`
+--
+
+INSERT INTO `predicto_usershare` (`id`, `user`, `sharecontent`, `total`, `prediction`) VALUES
+(1, 1, 'share Content', '100', 1),
+(2, 4, 'demo content', '1000', 1),
+(3, 5, 'Demo share content', '1666', 1);
 
 -- --------------------------------------------------------
 
@@ -269,7 +345,17 @@ CREATE TABLE IF NOT EXISTS `predicto_usersharehash` (
   `usershare` int(11) NOT NULL,
   `predictionhash` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+
+--
+-- Dumping data for table `predicto_usersharehash`
+--
+
+INSERT INTO `predicto_usersharehash` (`id`, `usershare`, `predictionhash`) VALUES
+(1, 1, 1),
+(2, 2, 2),
+(8, 3, 1),
+(9, 3, 2);
 
 -- --------------------------------------------------------
 
@@ -313,6 +399,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `socialid` varchar(255) NOT NULL,
   `logintype` int(11) NOT NULL,
   `json` text NOT NULL,
+  `points` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
@@ -320,14 +407,14 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `password`, `email`, `accesslevel`, `timestamp`, `status`, `image`, `username`, `socialid`, `logintype`, `json`) VALUES
-(1, 'wohlig', 'a63526467438df9566c508027d9cb06b', 'wohlig@wohlig.com', 1, '0000-00-00 00:00:00', 1, NULL, '', '', 0, ''),
-(4, 'pratik', '0cb2b62754dfd12b6ed0161d4b447df7', 'pratik@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, 'pratik', '1', 1, ''),
-(5, 'wohlig123', 'wohlig123', 'wohlig1@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, '', '', 0, ''),
-(6, 'wohlig1', 'a63526467438df9566c508027d9cb06b', 'wohlig2@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, '', '', 0, ''),
-(7, 'Avinash', '7b0a80efe0d324e937bbfc7716fb15d3', 'avinash@wohlig.com', 1, '2014-10-17 06:22:29', 1, NULL, '', '', 0, ''),
-(9, 'avinash', 'a208e5837519309129fa466b0c68396b', 'a@email.com', 2, '2014-12-03 11:06:19', 3, '', '', '123', 1, 'demojson'),
-(13, 'aaa', 'a208e5837519309129fa466b0c68396b', 'aaa3@email.com', 3, '2014-12-04 06:55:42', 3, NULL, '', '1', 2, 'userjson');
+INSERT INTO `user` (`id`, `name`, `password`, `email`, `accesslevel`, `timestamp`, `status`, `image`, `username`, `socialid`, `logintype`, `json`, `points`) VALUES
+(1, 'wohlig', 'a63526467438df9566c508027d9cb06b', 'wohlig@wohlig.com', 1, '0000-00-00 00:00:00', 1, NULL, '', '', 0, '', ''),
+(4, 'pratik', '0cb2b62754dfd12b6ed0161d4b447df7', 'pratik@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, 'pratik', '1', 1, '', ''),
+(5, 'wohlig123', 'wohlig123', 'wohlig1@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, '', '', 0, '', ''),
+(6, 'wohlig1', 'a63526467438df9566c508027d9cb06b', 'wohlig2@wohlig.com', 1, '2014-05-12 06:52:44', 1, NULL, '', '', 0, '', ''),
+(7, 'Avinash', '7b0a80efe0d324e937bbfc7716fb15d3', 'avinash@wohlig.com', 1, '2014-10-17 06:22:29', 1, NULL, '', '', 0, '', ''),
+(9, 'avinash', 'a208e5837519309129fa466b0c68396b', 'a@email.com', 2, '2014-12-03 11:06:19', 3, '', '', '123', 1, 'demojson', ''),
+(13, 'aaa', 'a208e5837519309129fa466b0c68396b', 'aaa3@email.com', 3, '2014-12-04 06:55:42', 3, NULL, '', '1', 2, 'userjson', '');
 
 -- --------------------------------------------------------
 
